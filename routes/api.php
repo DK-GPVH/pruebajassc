a47a9372c9d5ba9d4d24f47afcd424f94e71e405
Gerson Paul <ventura.hurtado.gerson.2002@gmail.com>
2023-06-21 16:26:30 -0500

    V1 estadisticas

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\propiedadesController;
 use App\Http\Controllers\API\clientesController;
 use App\Http\Controllers\API\categoriasController;
-use App\Http\Controllers\API\pagosController;
+use App\Http\Controllers\API\pagoscontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ Route::get('clientes/{id}',[clientesController::class,'show']);
 Route::get('propiedades/{manzana}/{lote}/{nrodesuministro}',[propiedadesController::class,'llamarpropiedad']);
 Route::get('categorias',[categoriasController::class,'index']);
 Route::get('categorias/{id}',[categoriasController::class,'show']);
-Route::get('estadistica',[pagosController::class,'estadistica']);
-Route::get('balancemensual',[pagosController::class,'balancemensual']);
-Route::get('balancetrimestral',[pagosController::class,'balancetrimestral']);
+Route::get('estadistica',[pagoscontroller::class,'estadistica']);
+Route::get('balancemensual',[pagoscontroller::class,'balancemensual']);
+Route::get('balancetrimestral',[pagoscontroller::class,'balancetrimestral']);
 
